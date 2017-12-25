4c5ea26a8a94856a816b0544e1c2b9df804dcdb6
Luc Murakami <luc.murakami@hotmail.com>
2017-12-24 19:27:48 -0800

    New Layout

---
+++ realTimeRides/realTimeRides/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  realTimeRides
+//
+//  Created by Luc Murakami on 2017-12-24.
+//  Copyright © 2017 VictorLuc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
