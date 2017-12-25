57a8c5d712365f391dfcaa9856271790813184b1
Luc Murakami <luc.murakami@hotmail.com>
2017-12-24 19:08:07 -0800

    First Commit

---
+++ Real Time Rides/Real Time Rides/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Real Time Rides
+//
+//  Created by Luc Murakami on 2017-12-24.
+//  Copyright © 2017 VictorLuc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
