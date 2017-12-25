0cb92d69bad7d7d3ab18429d2c2bdb6219b5cb05
Luc Murakami <luc.murakami@hotmail.com>
2017-12-24 20:43:19 -0800

    Added tab views

--- realTimeRides/realTimeRides/FirstViewController.swift
+++ realTimeRides/realTimeRides/FirstViewController.swift
@@ -8,8 +8,25 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var list = ["Buy milk", "Run 5 miles", "Get Peter", "Plant plants"]
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (list.count)
+    }
 
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return (cell)
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
