44a5685e9ebd8723874b48c3afdda9a98c0f8415
Luc Murakami <luc.murakami@hotmail.com>
2017-12-25 15:44:00 -0800

    Added custom list view

--- realTimeRides/realTimeRides/MyRidesViewController.swift
+++ realTimeRides/realTimeRides/MyRidesViewController.swift
@@ -7,35 +7,46 @@
 //
 
 import UIKit
+var lists: [[String]] = [["Richmond", "5", "$10", "Jan 1, 10:30am", "Paul Newman"], ["Vancouver", "2", "$20", "Dec 31, 8:20am", "Sebastian Vettel"]]
+var counter = 0
 
 class MyRidesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var list = ["Buy milk", "Run 5 miles", "Get Peter", "Plant plants"]
     
     @IBOutlet weak var myTableView: UITableView!
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return (list.count)
+        return (lists.count)
     }
 
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
-        cell.textLabel?.text = list[indexPath.row]
-        
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath) as! TableViewCell
+        cell.commonInit(lists[counter][0], seats: lists[counter][1], price: lists[counter][2], depart: lists[counter][3], user: lists[counter][4])
+        if (counter < lists.count - 1) {
+        counter += 1
+        }
         return (cell)
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == UITableViewCellEditingStyle.delete {
-            list.remove(at: indexPath.row)
+            lists.remove(at: indexPath.row)
+            counter = 0
             myTableView.reloadData()
         }
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        myTableView.reloadData()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "My Rides"
+        
+        let nibRide = UINib(nibName: "TableViewCell", bundle: nil)
+        myTableView.register(nibRide, forCellReuseIdentifier: "tableViewCell")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ realTimeRides/realTimeRides/myTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  myTableCellTableViewCell.swift
+//  realTimeRides
+//
+//  Created by Luc Murakami on 2017-12-25.
+//  Copyright © 2017 VictorLuc. All rights reserved.
+//
+
+import UIKit
+
+class myTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
