077e3bea4a23490456b167b3b48f1fd67748b38e
Luc Murakami <luc.murakami@hotmail.com>
2017-12-31 16:36:58 -0800

    Added Firebase Pods

---
+++ realTimeRides/Pods/Target Support Files/Pods-realTimeRidesTests/Pods-realTimeRidesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_realTimeRidesTests : NSObject
+@end
+@implementation PodsDummy_Pods_realTimeRidesTests
+@end
---
+++ realTimeRides/Pods/Target Support Files/Pods-realTimeRidesUITests/Pods-realTimeRidesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_realTimeRidesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_realTimeRidesUITests
+@end
